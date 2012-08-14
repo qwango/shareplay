be2dfe9f536790cdccbfd601eecf4cc6c95a6b01
Rob Goldsmith <robgoldsmith_gmail>
2012-08-14 10:46:58 +0100

    blah

--- test.py
+++ test.py
@@ -11,10 +11,12 @@ c = pymongo.Connection()
 
 db = c.db1
 companies =db.companies
+company = dict(tidm='VOD', name='Vodafone Group PLC', keywords=['Vodafone', 'Group'])
+companies.insert(company)
 print db.collection_names()
 count = 0
 skip=0
 all_companies = []
 t=time.time()
-print "Hello"
+print "Hello"*5
             
